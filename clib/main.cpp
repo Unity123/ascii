d159817df1efe64eb02eb534bb850f318bb689b5
Unity123 <38165550+Unity123@users.noreply.github.com>
2022-06-15 12:02:51 -0600

    Initial Commit

---
+++ clib/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  clib
+//
+//  Created by Lucas Greer on 6/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
