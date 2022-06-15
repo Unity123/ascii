3a3c16ea91cb3b1ed457d96e1bcc8864db1e8428
Unity123 <38165550+Unity123@users.noreply.github.com>
2022-06-15 13:01:18 -0600

    uuiuii

--- clib/main.cpp
+++ clib/main.cpp
@@ -5,10 +5,41 @@
 //  Created by Lucas Greer on 6/15/22.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <curses.h>
+#include <ncurses.h>
+
+void printatpos(int x, int y) {
+    mvaddch(x+1, y+1, '$');
+    refresh();
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    initscr();
+    curs_set(0);
+    addstr("##################\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n#                #\n##################\n"); // this has tiny holes in it and I don't know why
+    FILE* f = fopen("/dev/urandom", "r");
+    unsigned char* buf = (unsigned char*)malloc(64);
+    fread(buf, 1, 64, f);
+    fclose(f);
+    std::vector<int> xs;
+    std::vector<int> ys;
+    for (int i = 0; i < 64; i++) {
+        int x = buf[i]/16;
+        int y = buf[i]%16;
+        xs.push_back(x);
+        ys.push_back(y);
+    }
+    for (int i = 0; i < 64; i++) {
+        printatpos(xs[i], ys[i]);
+    }
+    getchar();
+    curs_set(1);
+    free(buf);
+    endwin();
     return 0;
 }
